fa18c5d4147dd0fc755d2fd07c1fd005ab5d15e8
Isa <isaiahweaver@protonmail.com>
2023-10-28 00:51:23 -0400

    added timer code

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft":5, "Medium":7, "Hard":12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        
+        startTimer(inputTime: eggTimes[sender.currentTitle!] ?? 0)
+        
+        
+    }
+    
+    func showTitle(buttonTitle: String) {
+        if (eggTimes.keys.contains(buttonTitle)){
+            print(eggTimes[buttonTitle] ?? "Error")
+        }
+        
+    }
+    
+    func startTimer(inputTime: Int) {
+        Timer.invalidate()
+        var time = inputTime * 60
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if time > 0 {
+                    print ("\(time) seconds")
+                    time -= 1
+                } else {
+                    Timer.invalidate()
+                }
+            }
+    }
+    
 
 
 }
